9c873e83ee2e52a985ddb0be2e10f253af14912d
Petr Zakharov <zakharovp11@gmail.com>
2020-12-30 00:06:35 +0300

    ok

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import time
 
@@ -7,48 +8,76 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')  # noqa
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'  # noqa
+PRAKTIKUM_OAUTH = {'Authorization': 'OAuth ' + PRAKTIKUM_TOKEN}  # noqa
+TG_BOT = telegram.Bot(token=TELEGRAM_TOKEN)
+
+MESSAGES = {'statuses': {'rejected': 'К сожалению в работе нашлись ошибки.',
+                         'approved': ('Ревьюеру всё понравилось, '
+                                      'можно приступать к следующему уроку.'),
+                         'default_error': 'Невозможно получить информацию'},
+            'success_message': 'У вас проверили работу "{key_1}"!\n\n{key_2}'
+            }
+
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    try:
+        homework_name = homework['homework_name']
+        homework_status = homework['status']
+        logging.info(f'Получен статус работы {homework_status}')
+    except KeyError as e:
+        logging.error(f'В апи практикум поменялся формат данных, {e}')
+        return MESSAGES['statuses']['default_error']
+    verdict = MESSAGES['statuses'][homework_status]
+    return MESSAGES['success_message'].format(key_1=homework_name,
+                                              key_2=verdict)
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    params = {'from_date': current_timestamp}
+    homework_statuses = requests.get(PRAKTIKUM_URL,
+                                     params=params, headers=PRAKTIKUM_OAUTH)
     return homework_statuses.json()
+    # тут должна быть безопасная обработка запроса и логирование
 
 
-def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+def send_message(message, bot_client=TG_BOT):
+    logging.info(f'Бот отправляет сообщение: {message}, chat_id {CHAT_ID} ')
+    return bot_client.send_message(CHAT_ID, message)
 
 
 def main():
-    # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
-
+    current_timestamp = 0
+    # current_timestamp = int(time.time())  # начальное значение timestamp
     while True:
         try:
-            new_homework = get_homework_statuses(current_timestamp)
+            new_homework = get_homework_statuses(
+                current_timestamp)  # получает json
+            logging.info('Яндекс практикум отдал ответ')
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0]))
+                logging.info('Бот успешно отправил сообщение')
+            current_timestamp = new_homework.get('current_date',
+                                                 current_timestamp)
+            logging.info(f'Смена времени отсчета. Новая итерация.')
+            time.sleep(300)
 
         except Exception as e:
             print(f'Бот столкнулся с ошибкой: {e}')
+            logging.critical(e)
             time.sleep(5)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        filename=__file__.split('.')[0] + '_log.log',
+        format='%(asctime)s:%(levelname)s:%(funcName)s %(message)s',
+        filemode='w'
+    )
     main()
